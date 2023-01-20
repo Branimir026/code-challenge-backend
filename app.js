b61c294b4db35675cc92aefd074fdb220cd0d302
Branimir026 <br.belic@gmail.com>
2023-01-20 17:15:38 +0100

    imported body parser

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 const login = require('./login')
 
 app.use(express.json())
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.post('/login', login)
 
